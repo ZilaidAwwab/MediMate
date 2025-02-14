8e4a68b041e6314bb730b7f1cfe6dd664f7d4d2c
ZilaidAwwab <zilaidawwab786@gmail.com>
2025-02-14 08:06:14 +0500

    Initial Commit

---
+++ Backend/src/main/java/com/deepseek/medimate/MediMateApplication.java
@@ -0,0 +1,13 @@
+package com.deepseek.medimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediMateApplication.class, args);
+    }
+
+}

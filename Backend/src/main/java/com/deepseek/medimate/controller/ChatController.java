30d220eda3755ef925d961b24c0fd06419363140
ZilaidAwwab <zilaidawwab786@gmail.com>
2025-02-16 21:52:11 +0500

    CORS for Frontend Fixed

--- Backend/src/main/java/com/deepseek/medimate/controller/ChatController.java
+++ Backend/src/main/java/com/deepseek/medimate/controller/ChatController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://medi-mate-black.vercel.app/"})
 @RestController
 @RequestMapping("")
 public class ChatController {
